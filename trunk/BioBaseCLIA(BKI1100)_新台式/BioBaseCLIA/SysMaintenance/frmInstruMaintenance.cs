3f03b6184e78a2ecdbf8a38b64e3f853b6366b89
2514817800@qq.com <2514817800@qq.com@179c3e24-10cf-477a-8f1d-1e888165336c>
2020-12-23 07:00:18 +0000

    系统维护界面返回改成隐藏

--- trunk/BioBaseCLIA(BKI1100)_新台式/BioBaseCLIA/SysMaintenance/frmInstruMaintenance.cs
+++ trunk/BioBaseCLIA(BKI1100)_新台式/BioBaseCLIA/SysMaintenance/frmInstruMaintenance.cs
@@ -1205,7 +1205,7 @@ namespace BioBaseCLIA.SysMaintenance
         }
         private void fbtnReturn_Click(object sender, EventArgs e)
         {
-            this.Close();
+            this.Hide();
         }
 
         private void fbtnInstruDiagnost_Click(object sender, EventArgs e)
