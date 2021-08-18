92b882a925ef375db289ef61b730531f59d5474a
751414166 <751414166@qq.com>
2021-08-18 09:34:37 +0000

    在多个项目过期同时提醒的基础上添加过期继续实验

--- BioBaseCLIA(BKI1100)_R30/BioBaseCLIA/Run/frmWorkList.cs
+++ BioBaseCLIA(BKI1100)_R30/BioBaseCLIA/Run/frmWorkList.cs
@@ -3595,8 +3595,20 @@ namespace BioBaseCLIA.Run
                                         else if (DateTime.Now.Date.AddDays(-Convert.ToInt32(dtItemInfo.Rows[0][3])).Date > Convert.ToDateTime(ActiveDate))
                                         {
                                             //2018-07-31 zlx add
-                                            frmMsgShow.MessageShow(getString("btnWorkList.Text"), getString("keywordText.hiscalibrationOver"));
-                                            return false;
+                                            scalingInfo.ItemName = item.Key;
+                                            scalingInfo.RegenBatch = reBNum.Key;
+                                            scalingInfo.Num = "0";
+                                            scalingInfo.TestConc = dtItemInfo.Rows[0][1].ToString();
+                                            scalingInfo.testType = int.Parse(dtItemInfo.Rows[0][0].ToString());
+                                            lisScalingInfo.Add(scalingInfo);
+                                            scalingInfo = new ScalingInfo();
+                                            if (!flag)
+                                            {
+                                                ExpiredItems += getString("keywordText.ProjectName") + item.Key + "  " + getString("keywordText.Reagentbatch") + reBNum.Key + "\n";
+                                                flag = true;
+                                            }
+                                            else
+                                                ExpiredItems += getString("keywordText.ProjectName") + item.Key + "  " + getString("keywordText.Reagentbatch") + reBNum.Key + "\n";
                                         }
                                         else
                                         {
@@ -3665,6 +3677,14 @@ namespace BioBaseCLIA.Run
                                     }
                                     else if (DateTime.Now.Date.AddDays(-Convert.ToInt32(dtItemInfo.Rows[0][3])).Date > Convert.ToDateTime(ActiveDate))
                                     {
+                                        scalingInfo.ItemName = item.Key;
+                                        scalingInfo.RegenBatch = reBNum.Key;
+                                        scalingInfo.Num = "0";
+                                        scalingInfo.TestConc = dtItemInfo.Rows[0][1].ToString();
+                                        scalingInfo.testType = int.Parse(dtItemInfo.Rows[0][0].ToString());
+                                        lisScalingInfo.Add(scalingInfo);
+                                        scalingInfo = new ScalingInfo();
+                                        //2018-07-31 zlx add
                                         if (!flag)
                                         {
                                             ExpiredItems += getString("keywordText.ProjectName") + item.Key + "  " + getString("keywordText.Reagentbatch") + reBNum.Key + "\n";
